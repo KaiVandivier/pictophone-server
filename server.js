ff075c105f7319e5959abd422e1f3513a897e519
Kai Vandivier <kpvandivier@gmail.com>
2020-04-28 15:02:33 -0700

    Add a basic timer prototype

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 
+const { startTimer } = require("./lib/timer");
+
 const port = process.env.PORT || 4000;
 const app = express();
 const server = http.createServer(app);
@@ -11,7 +13,11 @@ io.on("connection", (socket) => {
   console.log("New client connected, id: ", socket.id);
 
   socket.emit("message", { message: "You connected successfully!" })
-  socket.emit("message", { message: "You connected successfully!" })
+
+  socket.on("start-timer", () => {
+    console.log("received start-timer event");
+    startTimer(socket, 90);
+  })
 
   socket.on("disconnect", (socket) => {
     console.log("Client disconnected")
