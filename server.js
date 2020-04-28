9778e0c8dbfbd24397fe95d844e7f13294d7cf04
Kai Vandivier <kpvandivier@gmail.com>
2020-04-28 13:08:23 -0700

    Get project started with simple socket connection

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const port = process.env.PORT || 4000;
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+  console.log("New client connected, id: ", socket.id);
+
+  socket.emit("message", { message: "You connected successfully!" })
+  socket.emit("message", { message: "You connected successfully!" })
+
+  socket.on("disconnect", (socket) => {
+    console.log("Client disconnected")
+  })
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send("Get received");
+})
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
