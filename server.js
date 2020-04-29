fff59e75e78e2d31b425ee70631087ad3ff41701
Kai Vandivier <kpvandivier@gmail.com>
2020-04-28 22:05:39 -0700

    test word import

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 
 const { startTimer } = require("./lib/timer");
+const { hardWords } = require("./lib/words");
 
 const port = process.env.PORT || 4000;
 const app = express();
@@ -13,6 +14,8 @@ io.on("connection", (socket) => {
   console.log("New client connected, id: ", socket.id);
 
   socket.emit("message", { message: "You connected successfully!" })
+
+  console.log("There are ", hardWords.length, " hard words");
 
   socket.on("start-timer", () => {
     console.log("received start-timer event");
